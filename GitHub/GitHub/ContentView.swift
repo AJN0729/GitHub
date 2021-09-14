2bcad3eeb0796a0f7f1b7552b4e563128f05d760
AJN0729 <90396810+AJN0729@users.noreply.github.com>
2021-09-14 10:09:11 -0500

    Add files via upload

---
+++ GitHub/GitHub/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GitHub
+//
+//  Created by AMStudent on 9/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("PUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .padding()
+            Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Button")/*@END_MENU_TOKEN@*/
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
